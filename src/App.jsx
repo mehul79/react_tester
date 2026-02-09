c5f76518fe1590b67e5bc9f030c399963a95ee01
Mehul Gupta <mehulrgupta@gmail.com>
2026-02-09 17:54:08 +0530

    added uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import {uuid} from 'uuidv4';
+
 export default function App() {
+
+  const newId = uuid();
+  const id= newId
+
   return (
     <div className="App">
       <h1>React App</h1>
+      <p>Assigned UUID: {id}</p>
     </div>
   );
 }
